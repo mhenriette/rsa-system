1833a9393f870bd9cd0947f17474f151f65f311c
mhenriette <mhenriette00@gmail.com>
2023-08-20 11:34:25 +0200

    feat: add home link in logo

--- src/components/navbar/DashboardNavbar.tsx
+++ src/components/navbar/DashboardNavbar.tsx
@@ -1,12 +1,13 @@
 import logo from "@/public/Logo.png";
 import Image from "next/image";
+import Link from "next/link";
 const DashboardNavbar = () => {
   return (
     <div className="flex justify-between p-5 items-center bg-purple-900">
-      <div className="flex items-center space-x-2">
+      <Link className="flex items-center space-x-2" href="/">
         <Image src={logo} width={32} height={32} alt="logo" />
         <p>Rwanda scouts association</p>
-      </div>
+      </Link>
       <div>profile</div>
     </div>
   );
