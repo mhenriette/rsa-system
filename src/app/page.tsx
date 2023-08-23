45349e7ebfa029184a300040ce0ddccdfba903dd
mhenriette <mhenriette00@gmail.com>
2023-08-23 18:04:06 +0200

    feat: add sharp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Navbar from "@components/sections/NavBar";
 import Hero from "@components/sections/Hero";
 import About from "@components/sections/About";
 import Footer from "@components/sections/Footer";
-import Wrapper from "@components/wrapper/wrapper";
 export default function Home() {
   return (
     <main className="text-white">
