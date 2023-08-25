d88189aa53a33b5ccf88089474cc7e15e56d8060
mhenriette <mhenriette00@gmail.com>
2023-08-26 01:39:10 +0200

    fix: clear dashboard

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,9 +1,5 @@
-import MemberIdCard from "@/components/certificate/MemberIdCertificate";
 import Members from "@/components/ui/Members";
-import { getUsers } from "@/lib/actions";
-
 const DashboardPage = async () => {
-  const usersWithNoId = await getUsers();
   return (
     <div className="p-5">
       <div className="flex gap-x-3 items-center">
@@ -11,18 +7,6 @@ const DashboardPage = async () => {
         <Members />
         <Members />
         <Members />
-      </div>
-      <div>
-        {usersWithNoId.map((user, key) => (
-          <h1 key={key}>{user.email}</h1>
-        ))}
-      </div>
-      <div className="bg-gray-400 p-10">
-        <MemberIdCard />
-
-        <button className="bg-orange-400 text-white m-5 p-5">
-          Dowload Certificate
-        </button>
       </div>
     </div>
   );
