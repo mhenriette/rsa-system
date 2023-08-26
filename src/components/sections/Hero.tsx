b10ef1a9745ce658fe99f3a188e923aeb9ca39eb
mhenriette <mhenriette00@gmail.com>
2023-08-26 13:26:37 +0200

    fix: remove lorem paragraph

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -6,14 +6,17 @@ const Hero = () => {
     <div className="bg-hero-pattern w-full bg-fill bg-cover relative bg-center">
       <div className="bg-black bg-opacity-30 w-full">
         <div className="py-20 px-5 md:py-40 md:pl-32">
-          <div className="p-8 md:p-16 flex flex-col md:w-2/5 bg-black bg-opacity-60">
+          <div className="p-8 md:p-16 flex flex-col md:w-[45%] bg-black bg-opacity-60">
             <h1 className="font-bold text-lg md:text-4xl mb-2 md:mb-5">
               Preparing young people with skills for life
             </h1>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-              saepe velit delectus laborum id excepturi iure eum amet quos
-              molestiae veniam alias.
+              The Rwanda Scouts Association is an educational movement for young
+              people, based on voluntary services. It is a Movement, which
+              remains non-political and does include all religions. It is open
+              to all without distinction of gender, origin, race or creed, in
+              accordance with the purpose, principles and scout method as
+              conceived by the Founder, Baden Powell.
             </p>
             <div className="flex flex-col sm:flex-row gap-5 items-center justify-center mt-5">
               <Button full="true">Volunteer</Button>
