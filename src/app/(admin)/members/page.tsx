0fa6d7fd058a2630084861cce76fbea21c8f9b93
mhenriette <mhenriette00@gmail.com>
2023-08-26 12:12:05 +0200

    fix: add members table

--- src/app/(admin)/members/page.tsx
+++ src/app/(admin)/members/page.tsx
@@ -1,16 +1,36 @@
-"use client";
-import Button from "@/components/ui/Button";
+import { getMembers } from "@/lib/actions";
+import MemberRow from "@/components/ui/MemberRow";
 import Link from "next/link";
-import { ReactElement } from "react";
+import plus from "@/public/plus.svg";
+import Image from "next/image";
 
-const page = (): ReactElement => {
+const page = async () => {
+  const members = await getMembers();
   return (
-    <div className="p-3">
-    <h1 className="text-green-500 font-medium text-3xl">Add a Member</h1>
-    <Link href={"/members/new"}>
-      <Button>Create new member</Button>
-    </Link>
-  </div>
+    <div className="p-3 pt-16">
+      <div className="flex justify-between items-center">
+        <h1 className="text-green-500 font-medium text-3xl">Members</h1>
+        <Link
+          href="/members/new"
+          className="bg-[#36043A] text-white py-2 px-4 rounded-3xl flex gap-1.5 items-center text-lg"
+        >
+          <Image src={plus} width={36} height={36} alt="plus" /> add new member
+        </Link>
+      </div>
+      <div className="text-black mt-9 bg-white px-5">
+        <div className="grid grid-cols-6 h-16 border-b border-b-gray-800 border-opacity-50 items-center">
+          <p className="font-medium">Name</p>
+          <p className="font-medium">Unit name</p>
+          <p className="font-medium">Address</p>
+          <p className="font-medium">District</p>
+          <p className="font-medium">Occupation</p>
+          <p className="font-medium">Contact</p>
+        </div>
+        {members.map((member) => (
+          <MemberRow key={member.id} member={member} />
+        ))}
+      </div>
+    </div>
   );
 };
 
