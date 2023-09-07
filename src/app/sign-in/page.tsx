1e3a7e79e575a57292ee499f4ebf56b4481e0912
Ntwali Edson <ntwaliedson9@gmail.com>
2023-09-07 09:59:24 +0200

    feat: improved the login.

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -3,34 +3,76 @@ import { login } from "@/lib/actions";
 import logo from "@/public/Logo.png";
 
 import Image from "next/image";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useLocalStorage } from "usehooks-ts";
 const page = () => {
+  const router = useRouter();
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [_, setUser] = useLocalStorage("token", "");
 
   async function onLogin(formData: FormData) {
-    const res = await login(formData);
-    setUser(JSON.stringify(res?.token));
-    redirect("/dashboard");
+    try {
+      const res = await login(formData);
+      setUser(JSON.stringify(res?.token));
+      router.push("/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gradient-to-b from-purple-400 to-purple-200">
-      <div className="flex flex-col p-5 space-y-5">
-        <div className="flex justify-center flex-col items-center">
-          <Image src={logo} width={52} height={52} alt="logo" />
-          <h1 className="text-xl font-bold text-center">
-            RWANDA SCOUTS ASSOCIATION
-          </h1>
-        </div>
-        <form action={onLogin}>
-          <input type="text" name="username" />
-          <input type="text" name="password" />
-          <button type="submit">submit</button>
+    <main className="relative flex flex-1 flex-col overflow-hidden px-4 py-8 sm:px-6 lg:px-8">
+      <img
+        src="/img/beams-cover@95.jpg"
+        alt=""
+        className="absolute left-1/2 top-0 -ml-[47.5rem] w-[122.5rem] max-w-none"
+      />
+      <div className="absolute inset-0 text-slate-900/[0.07] [mask-image:linear-gradient(to_bottom_left,white,transparent,transparent)]">
+        <Image src={logo} width={52} height={52} alt="logo" />
+      </div>
+      <div className="relative flex flex-1 flex-col items-center justify-center pb-16 pt-12">
+        <h1 className="mb-8 font-semibold">Log in to your RSA account</h1>
+        <form action={onLogin} className="w-full max-w-sm">
+          <div className="mb-6">
+            <label
+              htmlFor="username"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Username
+            </label>
+            <input
+              type="username"
+              id="username"
+              name="username"
+              className="mt-2 appearance-none text-slate-900 bg-white rounded-md block w-full px-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-slate-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-slate-200"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-sm font-semibold leading-6 text-gray-900"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="mt-2 appearance-none text-slate-900 bg-white rounded-md block w-full px-3 h-10 shadow-sm sm:text-sm focus:outline-none placeholder:text-slate-400 focus:ring-2 focus:ring-sky-500 ring-1 ring-slate-200"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="inline-flex justify-center rounded-lg text-sm font-semibold py-2.5 px-4 bg-[#4c1d95] text-white hover:bg-[#4c1d95] w-full"
+          >
+            <span>Sign in to account</span>
+          </button>
+          <input type="hidden" name="remember" defaultValue="true" />
         </form>
       </div>
-    </div>
+    </main>
   );
 };
 
