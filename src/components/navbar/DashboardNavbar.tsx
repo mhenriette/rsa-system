8924f908fa9f03d0d25dfe588d63b059819cc8cf
Ivad <yvesyvad@gmail.com>
2023-09-09 20:27:34 +0200

    feat: implement profile page

--- src/components/navbar/DashboardNavbar.tsx
+++ src/components/navbar/DashboardNavbar.tsx
@@ -3,6 +3,7 @@ import { AuthContext } from "@/store/authContext";
 import Image from "next/image";
 import Link from "next/link";
 import { useContext, useState } from "react";
+import { FaUserAlt } from "react-icons/fa";
 const DashboardNavbar = () => {
   const [open, setOpen] = useState(false);
   const handleClick = () => {
@@ -16,31 +17,25 @@ const DashboardNavbar = () => {
         <p className="text-white font-semibold">RSA</p>
       </Link>
       {userInfo?.user?.role === "hqadmin" && (
-        <div className="relative">
+        <div className="relative flex gap-5 items-center">
           <button
             onClick={handleClick}
             className="bg-white text-purple-950 font-bold  px-5 text-sm py-2 text-center rounded-md hover:bg-purple-700 hover:text-white"
           >
             Add new admin
           </button>
+          <Link href="/profile">
+            <FaUserAlt className="text-2xl" />
+          </Link>
           {open && (
             <div className="absolute text-gray-800 transform translate-y-2 gap-2 w-max shadow bg-gray-100 flex flex-col rounded p-5">
-              <Link
-                onClick={() => setOpen(false)}
-                href={"new-admin?type=hqadmin"}
-              >
+              <Link onClick={() => setOpen(false)} href={"new-admin?type=hqadmin"}>
                 Hq admin
               </Link>
-              <Link
-                onClick={() => setOpen(false)}
-                href={"new-admin?type=districtmanager"}
-              >
+              <Link onClick={() => setOpen(false)} href={"new-admin?type=districtmanager"}>
                 District manager
               </Link>
-              <Link
-                onClick={() => setOpen(false)}
-                href={"new-admin?type=unitleader"}
-              >
+              <Link onClick={() => setOpen(false)} href={"new-admin?type=unitleader"}>
                 Unit leader
               </Link>
             </div>
