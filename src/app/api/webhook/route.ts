2cb19521dc20fe53a6cb3b06638b10818f2a59da
izzett222 <iizzeddin62@gmail.com>
2023-09-10 21:47:47 +0200

    add webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,16 +1,34 @@
+import { prisma } from "@/lib/db";
+import { NextResponse } from "next/server";
+export async function POST(req: Request, res: Response) {
+    console.log("everything is working")
+  try {
+    console.log(req.body, '================')
+    const body = await req.json();
+    console.log(body, '================')
+    // const payment = await prisma.payments.findUnique({
+    //     where: {
+    //         ref: body.data.ref,
+    //     },
+    // });
+    // console.log(payment, '================');
 
-import {NextResponse} from "next/server"
-export async function GET(req:Request,res:Response) {
-    try{
+    await prisma.payments.update({
+        where: { ref: body.data.ref },
+        data: {
+            status: body.data.status,
+            processed_at: body.data.processed_at,
+        },
+    });
+    
     return NextResponse.json({
-        success: true, 
-    })
-    } catch (error) {
-        return NextResponse.json({
-            success:false,
-        })
-    }
-       
+      success: true,
+      message: "Payment webhook received and payment updated successfully"
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({
+      success: false,
+    });
+  }
 }
-
-
