66eac1e2c3eaff7d8991f08192e7b784d062a816
mhenriette <mhenriette00@gmail.com>
2023-09-10 22:34:31 +0200

    fix: log error

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,25 +1,38 @@
 import { prisma } from "@/lib/db";
 import { NextResponse } from "next/server";
 export async function POST(req: Request, res: Response) {
-    console.log("everything is working")
+  console.log("everything is working");
   try {
+    console.log(req.body, "================");
     const body = await req.json();
+    console.log(body, "================");
+    // const payment = await prisma.payments.findUnique({
+    //     where: {
+    //         ref: body.data.ref,
+    //     },
+    // });
+    // console.log(payment, '================');
+
     await prisma.payments.update({
-        where: { ref: body.data.ref },
-        data: {
-            status: body.data.status,
-            processed_at: new Date(body.data.processed_at),
-        },
+      where: { ref: body.data.ref },
+      data: {
+        status: body.data.status,
+        processed_at: new Date(body.data.processed_at),
+      },
     });
-    
+
     return NextResponse.json({
       success: true,
-      message: "Payment webhook received and payment updated successfully"
+      message: "Payment webhook received and payment updated successfully",
     });
-  } catch (error) {
+  } catch (error:any) {
     console.log(error);
-    return NextResponse.json({
-      success: false,
-    });
+    return NextResponse.json(
+      {
+        success: false,
+        message: error.message
+      },
+      { status: 500 }
+    );
   }
 }
