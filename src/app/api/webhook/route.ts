3a55ed164f58ef978c0fe5c6f71a8c5eceb96201
izzett222 <iizzeddin62@gmail.com>
2023-09-10 22:12:29 +0200

    fix update payment

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -3,21 +3,12 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request, res: Response) {
     console.log("everything is working")
   try {
-    console.log(req.body, '================')
     const body = await req.json();
-    console.log(body, '================')
-    // const payment = await prisma.payments.findUnique({
-    //     where: {
-    //         ref: body.data.ref,
-    //     },
-    // });
-    // console.log(payment, '================');
-
     await prisma.payments.update({
         where: { ref: body.data.ref },
         data: {
             status: body.data.status,
-            processed_at: body.data.processed_at,
+            processed_at: new Date(body.data.processed_at),
         },
     });
     
